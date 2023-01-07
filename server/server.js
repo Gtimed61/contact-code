64474e28ffce6ba13c01b900c5d50ede824b6f82
Grant Thorne <gthorne61@gmail.com>
2023-01-07 15:07:41 -0600

    concurrently

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('../client/'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/html-routes.js')(app);
+
+app.listen (PORT, () => function () {
+    console.log(`App listening on PORT: ${PORT}`);
+});
+
+
